e67e3f6de532051a03fa96db55c03da5bfc6ccee
Nicolas <nicolasguardattiutn@gmail.com>
2025-12-30 13:45:27 -0300

    .

--- hospital-staff-manager/src/pages/Crud/Appointment.tsx
+++ hospital-staff-manager/src/pages/Crud/Appointment.tsx
@@ -77,7 +77,7 @@ const Appointment: React.FC = () => {
                         draggable: true
                     });
 
-                    navigate('/turnos')
+                    navigate(-1)
 
                 }
 
